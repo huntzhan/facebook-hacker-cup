869347e89687c283ffc3f6c2b439675c4e35db79
huntzhan <programmer.zhx@gmail.com>
2016-01-08 13:52:19 +0800

    init.

---
+++ cpp-template/solution.cc
@@ -0,0 +1,48 @@
+// {{{ $VIMCODER$ <-----------------------------------------------------
+// vim:filetype=cpp:foldmethod=marker:foldmarker={{{,}}}
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+// }}}
+
+const string kInputFilename = "";
+const string kOutputFilename = "output.txt";
+
+ifstream fin(kInputFilename);
+ofstream fout(kOutputFilename);
+
+
+
+int main() {
+  // deal with input.
+
+  // solve the probleam.
+
+  // deal with output.
+}
